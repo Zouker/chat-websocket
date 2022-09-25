6cbcd6ed01b40a0e8a661e499b6d9df3d684c490
Denis Shubin <denis.shubin@gmail.com>
2022-09-25 20:15:56 +0300

    feat: added name and scrolling of messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 import io from 'socket.io-client'
 
@@ -10,6 +10,9 @@ function App() {
     const [messages, setMessages] = useState<Array<any>>([])
 
     const [message, setMessage] = useState('hello')
+    const [name, setName] = useState('Denis')
+    const [isAutoScrollActive, setIsAutoScrollActive] = useState(true)
+    const [lastScrollTop, setLastScrollTop] = useState(0)
 
     useEffect(() => {
         socket.on('init-messages-published', (messages) => {
@@ -21,31 +24,62 @@ function App() {
         })
     }, [])
 
+    useEffect(() => {
+        if (isAutoScrollActive) {
+            messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'});
+        }
+    }, [messages])
+
+    const messagesAnchorRef = useRef<HTMLDivElement>(null)
+
     return (
         <div className="App">
             <div>
-                <div style={{
-                    border: '1px solid black',
-                    padding: '10px',
-                    height: '300px',
-                    width: '300px',
-                    overflowY: 'scroll'
-                }}>
+                <div
+                    className={'mainBlock'}
+                    onScroll={(e) => {
+                        let element = e.currentTarget
+                        let maxScrollPosition = element.scrollHeight - element.clientHeight
+
+                        if (element.scrollTop > lastScrollTop && Math.abs(maxScrollPosition - element.scrollTop) < 10) {
+                            setIsAutoScrollActive(true)
+                        } else {
+                            setIsAutoScrollActive(false)
+                        }
+                        setLastScrollTop(element.scrollTop)
+                    }}
+                >
                     {messages.map(m => {
                         return <div key={m.id}>
                             <b>{m.user.name}: </b> {m.message}
                             <hr/>
                         </div>
                     })}
+                    <div ref={messagesAnchorRef}>
+                    </div>
                 </div>
-                <textarea value={message}
-                          onChange={(e) => setMessage(e.currentTarget.value)}>
+                <div>
+                    <input className={'input'} value={name}
+                           onChange={(e) => setName(e.currentTarget.value)}/>
+                    <button onClick={() => {
+                        socket.emit('client-name-sent', name)
+                    }}>send name
+                    </button>
+                </div>
+                <div className={'newMessage'}>
+                <textarea
+                    className={'textArea'}
+                    value={message}
+                    onChange={(e) => setMessage(e.currentTarget.value)}>
             </textarea>
-                <button onClick={() => {
-                    socket.emit('client-message-sent', message)
-                    setMessage('')
-                }}>Send
-                </button>
+                    <button
+                        className={'sendButton'}
+                        onClick={() => {
+                            socket.emit('client-message-sent', message)
+                            setMessage('')
+                        }}>Send
+                    </button>
+                </div>
             </div>
         </div>
     );
