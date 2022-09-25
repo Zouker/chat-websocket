49a1b6b062da4be3c3bd6a1964bd002954a09313
Denis Shubin <denis.shubin@gmail.com>
2022-09-25 18:05:27 +0300

    init: start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import io from 'socket.io-client'
+
+// const socket = io('http://localhost:3009');
+const socket = io('https://chat-websocket-back.herokuapp.com');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [messages, setMessages] = useState<Array<any>>([])
+
+    const [message, setMessage] = useState('hello')
+
+    useEffect(() => {
+        socket.on('init-messages-published', (messages) => {
+            setMessages(messages)
+        })
+
+        socket.on('new-message-sent', (message) => {
+            setMessages((messages) => [...messages, message])
+        })
+    }, [])
+
+    return (
+        <div className="App">
+            <div>
+                <div style={{
+                    border: '1px solid black',
+                    padding: '10px',
+                    height: '300px',
+                    width: '300px',
+                    overflowY: 'scroll'
+                }}>
+                    {messages.map(m => {
+                        return <div key={m.id}>
+                            <b>{m.user.name}: </b> {m.message}
+                            <hr/>
+                        </div>
+                    })}
+                </div>
+                <textarea value={message}
+                          onChange={(e) => setMessage(e.currentTarget.value)}>
+            </textarea>
+                <button onClick={() => {
+                    socket.emit('client-message-sent', message)
+                    setMessage('')
+                }}>Send
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
